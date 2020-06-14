e4aad541fe166a74bd142aa84c316abdc52c6655
nikunjsingh93 <nikunj.singh@live.com>
2020-06-14 16:52:52 -0400

    firest commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Ng2ImgMaxService } from 'ng2-img-max';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  fileUrl;
+  uploadedImage: Blob;
+
   title = 'anamorphic-desqueeze';
+
+  constructor(private ng2ImgMax: Ng2ImgMaxService,public sanitizer: DomSanitizer) {
+
+  }
+
+  onImageChange(event) {
+    let image = event.target.files[0];
+  
+    this.ng2ImgMax.resizeImage(image, 400, 300).subscribe(
+      result => {
+        this.uploadedImage = new File([result], result.name);
+        this.downloadFile(this.uploadedImage);
+        console.log("IMG",this.uploadedImage)
+      },
+      error => {
+        console.log('😢 Oh no!', error);
+      }
+    );
+  }
+
+  downloadFile(data) {
+
+    const blob = new Blob([data], { type: 'data:image/jpeg;base64' });
+
+    this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Ng2ImgMaxModule } from 'ng2-img-max';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2ImgMaxModule
   ],
   providers: [],
   bootstrap: [AppComponent]

3aa962d54884c5d34059127c7a9cf219f578b20e
nikunjsingh93 <nikunj.singh@live.com>
2020-06-15 21:07:58 -0400

    few maybe addition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ export class AppComponent {
   fileUrl;
   uploadedImage: Blob;
 
+  imgSrc = '';
+
   title = 'anamorphic-desqueeze';
 
   constructor(public sanitizer: DomSanitizer) {
@@ -26,7 +28,9 @@ export class AppComponent {
     img = event.target.files[0];
 
 
-    this.compressImage(event.target.files[0],3840,919);
+    this.handleImage(event);
+
+    //this.compressImage(event.target.files[0],3840,919);
 
 
   }
@@ -53,6 +57,72 @@ export class AppComponent {
       
     
   }
+
+
+  handleImage(e){
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    var reader = new FileReader();
+
+
+    
+    reader.onload = (event) => {
+    console.log("in reader onload");
+        var img = new Image();
+        img.onload = () => {
+        console.log("in img onload");
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img,0,0);
+        }
+        console.log("in strr",event.target.result);
+        img.src = event.target.result as string;
+        this.imgSrc = img.src;
+    }
+    console.log("in down read",reader);
+    reader.readAsDataURL(e.target.files[0]);  
+    if (this.imgSrc != ''){
+      console.log("Heha")
+      this.downloadFile(this.b64toFile(this.imgSrc));
+    }
+   
+    
+}
+
+
+
+
+  b64toFile(dataURI): File {
+    // convert the data URL to a byte string
+    const byteString = atob(dataURI.split(',')[1]);
+
+    // pull out the mime type from the data URL
+    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+    // Convert to byte array
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    // Create a blob that looks like a file.
+    const blob = new Blob([ab], { 'type': mimeString });
+    blob['lastModifiedDate'] = (new Date()).toISOString();
+    blob['name'] = 'file';
+        
+    // Figure out what extension the file should have
+    switch(blob.type) {
+        case 'image/jpeg':
+            blob['name'] += '.jpg';
+            break;
+        case 'image/png':
+            blob['name'] += '.png';
+            break;
+    }
+    // cast to a File
+    return <File>blob;
+}
 
 
 
